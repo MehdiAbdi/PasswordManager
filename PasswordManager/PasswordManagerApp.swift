58b4466a26616d2c23df72d69e4f21c6ade12184
Mehdi <Appleseed.me77@gmail.com>
2022-01-20 18:00:29 +0330

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Mehdi on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
